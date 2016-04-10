63ef2a3962c896aec54b46ec9345da8e942f5b4e
Gilad Melamed <giladm@il.ibm.com>
2016-04-10 08:59:41 +0300

    minor changes

--- scripts/pairs/runSomething.sh
+++ scripts/pairs/runSomething.sh
@@ -8,21 +8,30 @@ if [ $1 ]
 then   
         baseAppKeyI="$1"
 else
-  echo usage $0 appKey javascript.js
-  exit ;
+  echo usage $0 appKey javascript.js dryrun=true/false 
+  exit 1;
 fi
 
-if [ $2 ]
-then   
+if [[ $2 ]] && [[ -r $2 ]]
+then
         javascriptFile="$2"
 else
-  echo usage $0 appKey javascript_file.js
-  exit ;
+  echo File $2 does not exist
+  echo usage $0 appKey javascript.js dryrun=true/false 
+  exit 2;
 fi
 
+if [ $3 ]
+then    
+        dryrunI="$3"
+else    
+        dryrunI=true
+fi      
+        
 echo Start `date`
-
-baseAppKeyI_eval="var baseAppKeyI='"$baseAppKeyI"'"
-
-/opt/mongodb/bin/mongo --eval "$baseAppKeyI_eval"  ./$javascriptFile
+             
+varEvals="var baseAppKeyI='"$baseAppKeyI"', dryrunI='"$dryrunI"'"
+echo $varEvals 
+        
+/opt/mongodb/bin/mongo --eval "$varEvals"  ./$javascriptFile
 
--- scripts/runscreen.sh
+++ scripts/runscreen.sh
@@ -12,10 +12,14 @@ screen -S "gilad" -X screen; screen -S "gilad" -p 5 -X title "db"
 #cd /Users/gilad/workspace/any-java-projects/xwebservice/push
 
 # ssh -L 3306:localhost:3306 devjbx01 ssh -L 3306:localhost:3306 -N xtify-dev@prd.db25.ec2.xtify.com
+# ssh -L 3306:localhost:3306 devjbx01 ssh -L 3306:localhost:3306 -N xtify-dev@qa20mysql01
+# ssh -L 27017:localhost:27017 devjbx01 ssh -L 27017:localhost:27017 -N xtify-dev@qa20mongodb01
+# ssh -L 3306:localhost:3306 devjbx01 ssh -L 3306:localhost:3306 -N xtify-dev@p20usmy01
 
 #bluemix i
 # export VCAP_APP_PORT=5000 ; python /Users/gilad/workspace/bluemix/sync/salestool/server.py # start local web server on port 5000
 # mongod --dbpath /Users/gilad/Documents/data/db/ # run mongodb on local
 #cd /Users/gilad/workspace/bluemix/sync/salestool
+# cf logs watsonxtify # check runtime web log
 
-#
+#cd /Users/gilad/Documents/Customers/ICICI/iphone1213/Sample-iMobile/native_Production/wls62Wls62Iphone.xcodeproj
